c59c08fac65128102cdc3ba49a74661e544e5474
David <dakinola54@gmail.com>
2024-01-08 05:03:36 -0800

    joining two lists

--- list.py
+++ list.py
@@ -5,3 +5,11 @@ fruits.append('pineapple')
 
 print(fruits[3 :])
 print(fruits[2 : 6])
+
+#list Manipulation 
+
+first = [1, 3, 5, 7, 9, 11]
+second = [2, 4, 6, 8, 10, 12]
+
+third = first.__add__(second)
+print(third)
