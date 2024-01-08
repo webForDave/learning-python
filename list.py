af3dc0d753c8b94ba147eba01b01404f55abc54f
David <dakinola54@gmail.com>
2024-01-08 05:07:09 -0800

    prints first list three times

--- list.py
+++ list.py
@@ -3,8 +3,8 @@
 fruits = ['orange', 'banana', 'apple', 'cherry', 'plum', 'mango', 'berry']
 fruits.append('pineapple')
 
-print(fruits[3 :])
-print(fruits[2 : 6])
+# print(fruits[3 :])
+# print(fruits[2 : 6])
 
 #list Manipulation 
 
@@ -12,4 +12,5 @@ first = [1, 3, 5, 7, 9, 11]
 second = [2, 4, 6, 8, 10, 12]
 
 third = first.__add__(second)
-print(sorted(third))
+# print(sorted(third))
+print(first * 3)
