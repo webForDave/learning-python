8616739a6ba4a895df852810559921efb582a55a
David <dakinola54@gmail.com>
2024-01-08 04:58:31 -0800

    added new items to list and prints items at specified index

--- list.py
+++ list.py
@@ -1,6 +1,7 @@
 #Basic List Operation 
 
-fruits = ['orange', 'banana', 'apple', 'cherry', 'plum']
+fruits = ['orange', 'banana', 'apple', 'cherry', 'plum', 'mango', 'berry']
 fruits.append('pineapple')
 
 print(fruits[3 :])
+print(fruits[2 : 6])
