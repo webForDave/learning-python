1767ca32980a4927ac03267caaa68f854f10c1e9
David <dakinola54@gmail.com>
2024-01-08 05:13:17 -0800

    nested list

--- list.py
+++ list.py
@@ -3,8 +3,8 @@
 fruits = ['orange', 'banana', 'apple', 'cherry', 'plum', 'mango', 'berry']
 fruits.append('pineapple')
 
-# print(fruits[3 :])
-# print(fruits[2 : 6])
+print(fruits[3 :])
+print(fruits[2 : 6])
 
 #list Manipulation 
 
@@ -14,3 +14,11 @@ second = [2, 4, 6, 8, 10, 12]
 third = first.__add__(second)
 print(sorted(third))
 print(first * 3)
+
+# Nested List 
+nums = [
+    [1, 3, 5, 7, 10],
+    [2, 4, 6, 8, 10],
+    [5, 10, 15, 20, 25]
+]
+print(nums)
