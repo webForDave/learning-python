629f09d5dd05d28f4cdf2ef4bb85f0bf9d7d12b4
David <dakinola54@gmail.com>
2024-01-10 03:49:54 -0800

    removed first instance of song dictionary

--- dictionaries.py
+++ dictionaries.py
@@ -1,21 +1,3 @@
-# #  (
-# #     "The Dark Side of the Moon",
-# #     "Pink Floyd",
-# #     1973,
-# #     (
-# #         "Speak to Me",
-# #         "Breathe",
-# #         "On the Run",
-# #         "Time",
-# #         "The Great Gig in the Sky",
-# #         "Money",
-# #         "Us and Them",
-# #         "Any Colour You Like",
-# #         "Brain Damage",
-# #         "Eclipse"
-# #     )
-# #  )
-
 # song = {
 #     "title": "The dark side of the Moon",
 #     "artist": "Pink Floyd",
