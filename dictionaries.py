ced9251928d8a776a96c8615d87c89f880883bd3
David <dakinola54@gmail.com>
2024-01-09 21:40:09 -0800

    converts the tuple to a dictionary

---
+++ dictionaries.py
@@ -0,0 +1,17 @@
+#  (
+#     "The Dark Side of the Moon",
+#     "Pink Floyd",
+#     1973,
+#     (
+#         "Speak to Me",
+#         "Breathe",
+#         "On the Run",
+#         "Time",
+#         "The Great Gig in the Sky",
+#         "Money",
+#         "Us and Them",
+#         "Any Colour You Like",
+#         "Brain Damage",
+#         "Eclipse"
+#     )
+#  )
