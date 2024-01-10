140b092da24e3e48bc069bb2f1173da236bc65ca
David <dakinola54@gmail.com>
2024-01-09 21:49:30 -0800

    done

--- dictionaries.py
+++ dictionaries.py
@@ -15,3 +15,25 @@
 #         "Eclipse"
 #     )
 #  )
+
+song = {
+    "title": "The dark side of the Moon",
+    "artist": "Pink Floyd",
+    "year": '1973',
+
+    "songs": [
+        "Speak to Me",
+        "Breathe",
+        "On the Run",
+        "Time",
+        "The Great Gig in the Sky",
+        "Money",
+        "Us and Them",
+        "Any Colour You Like",
+        "Brain Damage",
+        "Eclipse"
+    ]
+
+}
+
+
