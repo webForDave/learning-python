41903f633b0233305da2447c078a7b938d41fd11
David <dakinola54@gmail.com>
2024-01-10 03:45:03 -0800

    one way of creating a dictionary

--- dictionaries.py
+++ dictionaries.py
@@ -1,8 +1,27 @@
-#  (
-#     "The Dark Side of the Moon",
-#     "Pink Floyd",
-#     1973,
-#     (
+# #  (
+# #     "The Dark Side of the Moon",
+# #     "Pink Floyd",
+# #     1973,
+# #     (
+# #         "Speak to Me",
+# #         "Breathe",
+# #         "On the Run",
+# #         "Time",
+# #         "The Great Gig in the Sky",
+# #         "Money",
+# #         "Us and Them",
+# #         "Any Colour You Like",
+# #         "Brain Damage",
+# #         "Eclipse"
+# #     )
+# #  )
+
+# song = {
+#     "title": "The dark side of the Moon",
+#     "artist": "Pink Floyd",
+#     "year": '1973',
+
+#     "songs": [
 #         "Speak to Me",
 #         "Breathe",
 #         "On the Run",
@@ -13,69 +32,59 @@
 #         "Any Colour You Like",
 #         "Brain Damage",
 #         "Eclipse"
-#     )
-#  )
+#     ]
 
-song = {
-    "title": "The dark side of the Moon",
-    "artist": "Pink Floyd",
-    "year": '1973',
-
-    "songs": [
-        "Speak to Me",
-        "Breathe",
-        "On the Run",
-        "Time",
-        "The Great Gig in the Sky",
-        "Money",
-        "Us and Them",
-        "Any Colour You Like",
-        "Brain Damage",
-        "Eclipse"
-    ]
-
-}
+# }
 
 
-my_dict = {
-    "mango": 5,
-    "apple": 10,
-    "berry": 2
-}
+# my_dict = {
+#     "mango": 5,
+#     "apple": 10,
+#     "berry": 2
+# }
 
-print(my_dict["berry"])
+# print(my_dict["berry"])
 
-my_dict["orange"] = 20
-my_dict["orange"] = 4
-del(my_dict["orange"])
-print(my_dict)
+# my_dict["orange"] = 20
+# my_dict["orange"] = 4
+# del(my_dict["orange"])
+# print(my_dict)
 
-for value in my_dict :
-    print(my_dict[value])
+# for value in my_dict :
+#     print(my_dict[value])
 
 
 
-if "mango" in my_dict :
-    print (True)
+# if "mango" in my_dict :
+#     print (True)
 
-else : 
-    print(False)
+# else : 
+#     print(False)
 
-print(len(my_dict))
+# print(len(my_dict))
 
-my_dict.clear()
-print(my_dict)
+# my_dict.clear()
+# print(my_dict)
 
 
-person = {
-    "name": "David",
-    "age": 19,
-    "ninjas" : {
-        "yoshi": "black",
-        "ryu": "Red"
-    }
-}
+# person = {
+#     "name": "David",
+#     "age": 19,
+#     "ninjas" : {
+#         "yoshi": "black",
+#         "ryu": "Red"
+#     }
+# }
 
-person["ninjas"]['dave'] = "Yellow"
+# person["ninjas"]['dave'] = "Yellow"
 
-print(person["ninjas"])
+# print(person["ninjas"])
+
+
+
+man = dict([
+    ("name", "David"),
+    ("age", 19),
+    ("country", "Nigeria")
+])
+print(man)
