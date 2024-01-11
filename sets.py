a214d1e562e557241ed4163d17770716299e3908
David <dakinola54@gmail.com>
2024-01-11 15:47:21 -0800

    addint multiple entries to a set with the update metho

--- sets.py
+++ sets.py
@@ -3,4 +3,7 @@ names = set()
 
 #adding elements to an empty set with the add method
 names.add('David')
+
+#adding multiple elements to a set using the update method
+names.update(['Kenise', 'John', "Bill"])
 print(names)
