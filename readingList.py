ac8c7dd8f5d23f70dae4cd126ff19938602b264b
David <dakinola54@gmail.com>
2024-01-13 14:12:42 -0800

    finished menu prompt

--- readingList.py
+++ readingList.py
@@ -1,1 +1,9 @@
 reading_list = []
+
+menu_prompt = """Please enter one of the following options...
+
+- 'a' to add a book
+- 'l' to list the books
+- 'q' to quit
+
+Please what would you like to do?"""
