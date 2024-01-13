4faa2c7fd6c4de7f2454ce05d66563aeb03b9c45
David <dakinola54@gmail.com>
2024-01-13 14:21:47 -0800

    allowing user interactions

--- readingList.py
+++ readingList.py
@@ -1,6 +1,6 @@
 reading_list = []
 
-menu_prompt = """Please enter one of the following options...
+menu_prompt = """Please enter one of the following options :
 
 - 'a' to add a book
 - 'l' to list the books
@@ -9,3 +9,15 @@ menu_prompt = """Please enter one of the following options...
 Please what would you like to do?"""
 
 selected_option = input(menu_prompt).strip().lower()
+
+while selected_option != 'q' :
+    if selected_option == 'a' :
+        print('Adding...')
+
+    elif selected_option == 'l' :
+        print('Displaying...')
+
+    else : 
+        print(f'Sorry, {selected_option} isn\'t a valid option')
+
+    selected_option = input(menu_prompt).strip().lower()
