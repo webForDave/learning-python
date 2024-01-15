d63f8ef5c0dcb374541803a3f7036f8e23693f26
David <dakinola54@gmail.com>
2024-01-14 17:52:29 -0800

    process string function

--- sope.py
+++ sope.py
@@ -2,3 +2,8 @@ def exponentiate(base, power) :
     return base ** power
 
 print(exponentiate(5, 2))
+
+def process_string(word) :
+    return word.lower().strip()
+
+print(process_string('  DAVID      '))
